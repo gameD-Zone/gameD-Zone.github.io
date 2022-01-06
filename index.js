de9e7e2eb1c86ebd4ed9c0344357791ef596249b
N0sBa <n0sba.learning@gmail.com>
2022-01-06 17:40:53 +0700

    update paddle

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var canvas = document.createElement("CANVAS");
 canvas.width = 300;
-canvas.height = 300;
+canvas.height = 500;
 canvas.style.borderStyle = "solid";
 canvas.style.borderWidth = "1px";
 document.body.appendChild(canvas);
@@ -13,24 +13,33 @@ class Ball {
         this.radius = 7.5;
         this.ballX = 20;
         this.ballY = 250;
-        this.ballVelocityX = 2;
-        this.ballVelocityY = -2;
+        this.ballVelocityX = 3;
+        this.ballVelocityY = -3;
     }   
 }
 
 class Paddle {
     constructor() {
         this.paddleW = 80;
-        this.paddleH = 15;
+        this.paddleH = 50;
         this.paddleX = (canvas.width-this.paddleW)/2;
-        this.paddleY = canvas.height-this.paddleH;
-        this.paddleVelocity = 4;
+        this.paddleY = canvas.height-this.paddleH-50;
+        this.paddleVelocity = 6;
     }    
+}
+
+class Brick {
+    
 }
 var ball = new Ball();
 var paddle = new Paddle();
 
 function draw() {
+    if (ball.ballY >= paddle.paddleY + paddle.paddleH) {
+        alert("GAME OVER!");
+        location.reload();
+    }
+
     ctx.clearRect(0,0,canvas.width,canvas.height);
     drawBall();
     ball.ballX += ball.ballVelocityX;
@@ -41,9 +50,28 @@ function draw() {
     if (ball.ballY + ball.ballVelocityY > canvas.height-ball.radius || ball.ballY + ball.ballVelocityY < 0+ball.radius) {
         ball.ballVelocityY = -ball.ballVelocityY;
     } 
+    console.log("ballY: " + ball.ballY)
 
     drawPaddle();
-    
+    if (ball.ballX >= paddle.paddleX && 
+        ball.ballX <= paddle.paddleX + paddle.paddleW && 
+        ball.ballY + ball.ballVelocityY >= paddle.paddleY - ball.radius) {
+            ball.ballVelocityY = -ball.ballVelocityY;
+    }
+
+    if (ball.ballY >= paddle.paddleY &&
+        ball.ballY <= paddle.paddleY + paddle.paddleH &&
+        ball.ballX < paddle.paddleX &&
+        ball.ballX + ball.ballVelocityX >= paddle.paddleX - ball.radius) {
+            ball.ballVelocityX = -ball.ballVelocityX;
+    }
+
+    if (ball.ballY >= paddle.paddleY &&
+        ball.ballY <= paddle.paddleY + paddle.paddleH &&
+        ball.ballX > paddle.paddleX + paddle.paddleW &&
+        ball.ballX + ball.ballVelocityX <= paddle.paddleX + paddle.paddleW + ball.radius) {
+            ball.ballVelocityX = -ball.ballVelocityX;
+    }
 }
 
 function drawBall() {
@@ -58,6 +86,10 @@ function drawPaddle() {
     ctx.rect(paddle.paddleX, paddle.paddleY, paddle.paddleW, paddle.paddleH);
     ctx.fillStyle = "red";
     ctx.fill();
+}
+
+function drawBrick() {
+
 }
 
 document.addEventListener("keydown", function(event){
@@ -77,6 +109,6 @@ document.addEventListener("keydown", function(event){
             paddle.paddleX = canvas.width-paddle.paddleW;
         }
     }
-})
+});
 
 
